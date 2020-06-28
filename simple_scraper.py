335794fbb58ee71f84145557fa8b84cf66160b5e
jeMATHfischer <jefischer@uni-potsdam.de>
2020-06-28 08:50:53 +0200

    comments and adjustments for simple_scraper

--- simple_scraper.py
+++ simple_scraper.py
@@ -5,12 +5,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
+file_path = '/home/jens/Documents_Ubuntu/'
+
 options = Options()
-#options.add_argument('--headless')
+#options.add_argument('--headless')   #remove comment to let it run in the background
 profile = webdriver.FirefoxProfile()
-profile.set_preference('browser.download.folderList', 2) # custom location
+profile.set_preference('browser.download.folderList', 2) 
 profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', '/home/jens/Documents_Ubuntu/')
+profile.set_preference('browser.download.dir', file_path)
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
 driver = webdriver.Firefox(firefox_profile=profile, options=options)
@@ -19,12 +21,20 @@ driver.get("https://stats.oecd.org/")
 begin = driver.find_element_by_id("browsethemes")
 begin = begin.text.split('\n')
 
-
-def tree_disolver(highest_level_names = begin):  
+def tree_disolver(highest_level_names = begin): 
+    '''
+    -> This function runs through the whole tree starting from a highest given level.
+    
+    -> Highest given level has to be visible after driver loards page. 
+    
+    -> Highest_level_names has to contain elements in begin. Has to be given as a single list.
+    '''
     if len(highest_level_names) != 0:
         for name in highest_level_names:
+            # replace html formating to python interpretable.
             name = name.replace("&nbsp;", "\u00a0")
-            parent_click = driver.find_elements_by_xpath('//li[contains(@class ,"t closed") and span[text()="{}"]]'.format(name))
+            single_path = '//li[contains(@class ,"t closed") and span[text()="{}"]]'.format(name)
+            parent_click = driver.find_elements_by_xpath(single_path)
             if len(parent_click) == 1:
                 parent_click = parent_click[0]
                 parent_click.click()
@@ -32,11 +42,14 @@ def tree_disolver(highest_level_names = begin):
                 for item in parent_click:
                     if item.is_displayed():
                         item.click()
-            increase_depth = driver.find_elements_by_xpath('//li[contains(@class ,"t opened") and span[text()="{}"]]/ul/li/span'.format(name))
+            increase_depth = driver.find_elements_by_xpath(single_path + '/ul/li/span')
             if name in [e.get_attribute("innerHTML") for e in increase_depth]:
-                double_increase_depth = driver.find_elements_by_xpath('//li[contains(@class ,"t opened") and span[text()="{}"]]/ul/li/span[text()="{}"]/ul/li/span'.format(name, name))
+                # Difficulties arise if parent has child with same name parent_name.
+                # The branch parent_name - parent_name has to be evaluated taking two steps.
+                double_path = '//li[contains(@class ,"t opened") and span[text()="{}"]]/ul/li/span[text()="{}"]'.format(name, name)# '//li[contains(@class ,"t opened") and span[text()="{}"]]/ul/li/span[text()="{}"]/ul/li/span'.format(name, name)
+                double_increase_depth = driver.find_elements_by_xpath(double_path + '/ul/li/span')
                 tree_disolver([e.get_attribute("innerHTML") for e in double_increase_depth])
-                parent_click = driver.find_elements_by_xpath('//li[contains(@class ,"t opened") and span[text()="{}"]]/ul/li/span[text()="{}"]'.format(name, name))
+                parent_click = driver.find_elements_by_xpath(double_path)
                 if len(parent_click) == 1:
                     parent_click = parent_click[0]
                     parent_click.click()
@@ -48,6 +61,15 @@ def tree_disolver(highest_level_names = begin):
                 tree_disolver([e.get_attribute("innerHTML") for e in increase_depth])
 
 def download_clicker(download_section):
+    '''
+    -> Navigates through open tree sections to the download dialog window.
+    
+    -> Applies to all full data sets in the given download section. 
+    
+    ->The download_section has to be visible and all sublevels need to be unfolded.
+    
+    -> Use only after tree_disolver.
+    '''
     ds_elements = driver.find_elements_by_xpath('//li[span[text() = "{}"]]//a[contains(@class ,"ds")]'.format(download_section))
     for e_ds in ds_elements:
         e_ds.click()
